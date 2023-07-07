d9ecb1863222a8c03e8c4798f04f9173f78303dd
Alan <alanstvn420@gmail.com>
2023-07-07 12:25:44 -0500

    Initial commit

---
+++ src/JPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class JPanel {
+    private JButton Guardar;
+    private JTextField NompreP;
+    private JTextField CedulaP;
+    private JTextField EdadP;
+    private JButton Cargar;
+    private JLabel Nombre;
+    private JLabel Cedula;
+    private JLabel Edad;
+}
