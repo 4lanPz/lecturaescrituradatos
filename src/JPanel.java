ead2bbb389fb42bc4e79de8c871fd1b60d127441
Alan <alanstvn420@gmail.com>
2023-07-07 12:47:58 -0500

    VERSION 2

--- src/JPanel.java
+++ src/JPanel.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
-public class JPanel {
+public class JPanel extends Container {
+    private JPanel Panel;
     private JButton Guardar;
     private JTextField NompreP;
     private JTextField CedulaP;
@@ -9,4 +16,27 @@ public class JPanel {
     private JLabel Nombre;
     private JLabel Cedula;
     private JLabel Edad;
-}
+
+    public JPanel() {
+    String filePath="datos.dat";
+    MiClase miObjeto=new MiClase("Juan",42);
+    Cargar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        }
+    });
+        Guardar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+    public static void main(String[] args) {
+        JFrame Panel = new JFrame("JPanel");
+        Panel.setContentPane(new JPanel().Panel);
+        Panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Panel.pack();
+        Panel.setVisible(true);
+    }}
